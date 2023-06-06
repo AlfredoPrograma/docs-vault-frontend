0f790228a43f37b406d3915c8d3a825ad18de654
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-06 19:56:19 -0400

    chores: set up config for absolute/alias importing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect, describe, it } from 'vitest';
 
-import App from '../src/App';
+import App from '@/App';
 
 describe('App', () => {
   it('renders headline', () => {
